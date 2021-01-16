f870f9f4f97b7db04aba3c262a8c818b1acb245f
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-16 21:42:01 +0100

    Refactors entities to work with EF

--- API/Entities/Attachment.cs
+++ API/Entities/Attachment.cs
@@ -2,13 +2,8 @@ namespace API.Entities
 {
     public class Attachment
     {
-        public Attachment(string name, byte[] data)
-        {
-            this.Name = name;
-            this.Data = data;
-        }
-
-        public string Name { get; private set; }
-        public byte[] Data { get; private set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Data { get; set; }
     }
 }
--- API/Entities/Transaction.cs
+++ API/Entities/Transaction.cs
@@ -5,29 +5,14 @@ namespace API.Entities
 {
     public class Transaction
     {
-        public Transaction(int id, decimal amount, string currency, DateTime date, string description,
-                           Account debitAccount, Account creditAccount, IEnumerable<Tag> tags,
-                           IEnumerable<Attachment> attachments)
-        {
-            this.Id = id;
-            this.Amount = amount;
-            this.Currency = currency;
-            this.Date = date;
-            this.Description = description;
-            this.DebitAccount = debitAccount;
-            this.CreditAccount = creditAccount;
-            this.Tags = tags;
-            this.Attachments = attachments;
-        }
-
-        public int Id { get; private set; }
-        public decimal Amount { get; private set; }
-        public string Currency { get; private set; }
-        public DateTime Date { get; private set; }
-        public string Description { get; private set; }
-        public Account DebitAccount { get; private set; }
-        public Account CreditAccount { get; private set; }
-        public IEnumerable<Tag> Tags { get; private set; }
-        public IEnumerable<Attachment> Attachments { get; private set; }
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public Account DebitAccount { get; set; }
+        public Account CreditAccount { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+        public IEnumerable<Attachment> Attachments { get; set; }
     }
 }
