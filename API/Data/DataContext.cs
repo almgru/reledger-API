1640239833856666d77ad55702fa056f8961a798
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-16 21:38:27 +0100

    Adds DataContext class
    - Adds DbContext class DataContext
    - Adds appsetting configuration for connection string
    - Injects DataContext as Dbcontext and uses appsettings config for
    connection string

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+    }
+}
