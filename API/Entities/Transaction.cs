262873645d7ca2821e9def74879b4cd0813f45cf
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-17 08:49:38 +0100

    Marks required entity fields

--- API/Entities/Transaction.cs
+++ API/Entities/Transaction.cs
@@ -1,18 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Entities
 {
     public class Transaction
     {
         public int Id { get; set; }
+
+        [Required]
         public decimal Amount { get; set; }
+
+        [Required]
         public string Currency { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
         public string Description { get; set; }
+
+        [Required]
         public Account DebitAccount { get; set; }
+
+        [Required]
         public Account CreditAccount { get; set; }
+
         public IEnumerable<Tag> Tags { get; set; }
+
         public IEnumerable<Attachment> Attachments { get; set; }
     }
 }
