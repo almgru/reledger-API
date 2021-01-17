f7ac13cc869e4300b853cb6c3c725c8c9eaac279
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-01-17 11:59:27 +0100

    Changes so account descendants are added
    Previously, only the account specified was added when POSTing to
    /api/accounts. After this change, if the account to add has descendants
    they are also added.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,8 +33,52 @@ namespace API.Controllers
         [HttpPost]
         public async Task AddAccount([FromBody] Account account)
         {
-            // TODO: Add all descendants as well
-            await context.Accounts.AddAsync(account);
+            // Store a copy of the original account string so we can use that as a starting point for descendants
+            string accountString = account.Name;
+            string original = accountString;
+            int rightMostDotIndex;
+
+            /*
+             * We start with by getting the rightmost ancestor (for example "A.B" in account string "A.B.C" ["A.B.C"
+             * is not an ancestor since it has no descendants]) and work our way up to more ancestral accounts
+             * (First "A.B", then the ancestor of "A.B": "A").
+             *
+             * For each ancestor we determine its descendants by using the original account string as a starting
+             * point and working our way upwards until the descendant has the same account name as the current
+             * ancestor (So the descendant of "A.B" becomes "A.B.C" and the descendant of "A" is first "A.B.C" and
+             * then "A.B").
+             */
+            while ((rightMostDotIndex = accountString.LastIndexOf(".")) != -1) // until no more ancestors
+            {
+                var descendants = new List<Account>();
+                var ancestor = new Account() { Name = accountString.Substring(0, rightMostDotIndex).Trim() };
+                var descendantString = original;
+
+                while (descendantString != ancestor.Name)
+                {
+                    /*
+                     * Commit changes so the next query will be able to find the current descendant if it exists in the
+                     * database.
+                     */
+                    await context.SaveChangesAsync();
+
+                    var descendant = await context.Accounts.SingleOrDefaultAsync(acc => acc.Name == descendantString);
+
+                    if (descendant == null)
+                    {
+                        descendant = new Account() { Name = descendantString };
+                        await context.Accounts.AddAsync(descendant);
+                    }
+
+                    descendants.Add(descendant);
+                    descendantString = descendantString.Substring(0, descendantString.LastIndexOf(".")).Trim();
+                }
+
+                ancestor.Descendants = descendants;
+                await context.AddAsync(ancestor);
+                accountString = accountString.Substring(0, rightMostDotIndex).Trim();
+            }
+
             await context.SaveChangesAsync();
         }
     }
