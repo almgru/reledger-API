4b0a372fce64e7941922cc518c3094806927ba6d
Daniel Alm Grundström <almgru+github@fastmail.com>
2022-02-12 22:35:44 +0100

    Adds missing migration after removal of account balance

---
+++ API/Data/Migrations/20220212213315_RemoveBalanceFromAccount.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class RemoveBalanceFromAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Accounts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
